eeced500986f59e24ccf601beaff956ab8d2100a
Natalia Maryshko <n.maryshko@gmail.com>
2025-06-01 08:03:19 -0500

    make build

---
+++ index.f761bbea.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".contacts__form");e.addEventListener("submit",function(t){t.preventDefault(),e.reset()}),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.f761bbea.js.map
